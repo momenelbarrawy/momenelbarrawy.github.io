cc9a2293eb771af8cf7396057a63ea478770e987
0x911 <momenelbarrawy@gmail.com>
2025-08-10 20:58:52 +0300

    Quartz sync: Aug 10, 2025, 8:58 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/momenelbarrawy",
+      "LinkedIn": "https://linkedin.com/in/momen-elbarrawy",
+      "Email":"mailto:momenelbarrawy@gmail.com",
     },
   }),
 }
